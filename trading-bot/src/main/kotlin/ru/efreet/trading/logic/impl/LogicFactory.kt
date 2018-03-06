d9cc26b4a32a6d550425ae9bd7290b84998dba74
Evgeniy Amosov <efreet@efreet.ru>
2018-03-06 09:32:37 +0300

    no commit

--- trading-bot/src/main/kotlin/ru/efreet/trading/logic/impl/LogicFactory.kt
+++ trading-bot/src/main/kotlin/ru/efreet/trading/logic/impl/LogicFactory.kt
@@ -4,13 +4,7 @@ import ru.efreet.trading.bars.XExtBar
 import ru.efreet.trading.exchange.BarInterval
 import ru.efreet.trading.exchange.Instrument
 import ru.efreet.trading.logic.BotLogic
-import ru.efreet.trading.logic.impl.macd.MacdLogic
-import ru.efreet.trading.logic.impl.sd.SdLogic
-import ru.efreet.trading.logic.impl.sd2.Sd2Logic
 import ru.efreet.trading.logic.impl.sd3.Sd3Logic
-import ru.efreet.trading.logic.impl.sd4.Sd4Logic
-import ru.efreet.trading.logic.impl.sd5.Sd5Logic
-import ru.efreet.trading.logic.impl.sdonly.SdOnlyLogic
 
 /**
  * Created by fluder on 21/02/2018.
@@ -19,13 +13,7 @@ class LogicFactory {
     companion object {
         fun <P> getLogic(name: String, instrument: Instrument, barInterval: BarInterval, bars: MutableList<XExtBar> = mutableListOf()): BotLogic<P> {
             return when (name) {
-                "macd" -> return MacdLogic("macd", instrument, barInterval, bars) as BotLogic<P>
-                "sd" -> return SdLogic("sd", instrument, barInterval, bars) as BotLogic<P>
-                "sd2" -> return Sd2Logic("sd2", instrument, barInterval, bars) as BotLogic<P>
                 "sd3" -> return Sd3Logic("sd3", instrument, barInterval, bars) as BotLogic<P>
-                "sd4" -> return Sd4Logic("sd4", instrument, barInterval, bars) as BotLogic<P>
-                "sd5" -> return Sd5Logic("sd5", instrument, barInterval, bars) as BotLogic<P>
-                "sdonly" -> return SdOnlyLogic("sdonly", instrument, barInterval, bars) as BotLogic<P>
                 else -> throw RuntimeException("Unknown logic ${name}")
             }
 
