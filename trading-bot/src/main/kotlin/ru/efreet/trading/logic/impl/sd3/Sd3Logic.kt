5026abe5bc59f31b19e95d53d652d97d3178c6cd
Evgeniy Amosov <efreet@efreet.ru>
2018-03-08 09:19:08 +0300

    no commit

--- trading-bot/src/main/kotlin/ru/efreet/trading/logic/impl/sd3/Sd3Logic.kt
+++ trading-bot/src/main/kotlin/ru/efreet/trading/logic/impl/sd3/Sd3Logic.kt
@@ -64,7 +64,7 @@ class Sd3Logic(name: String, instrument: Instrument, barInterval: BarInterval, b
 
     override fun metrica(stats: TradesStats): Double {
         val hours = Duration.between(stats.start, stats.end).toHours()
-        return BotLogic.fine(stats.trades.toDouble(), hours / 12.0, 4.0) + BotLogic.fine(stats.goodTrades, 2.0, 10.0) + BotLogic.fine(stats.sma10, 0.8, 10.0) + BotLogic.fine(stats.profit, 1.0) + stats.profit / 10.0
+        return BotLogic.fine(stats.trades.toDouble(), hours / 12.0, 4.0) + BotLogic.fine(stats.goodTrades, 2.0, 10.0) + BotLogic.fine(stats.sma10, 0.8, 10.0) + BotLogic.fine(stats.profit, 1.0) + stats.profit / 5.0
     }
 
     override fun copyParams(orig: SimpleBotLogicParams): SimpleBotLogicParams = orig.copy()
