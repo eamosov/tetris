36ac29ac81d206be673062930e01ffb671c3f8fd
efreet <efreet@efreet.ru>
2018-04-12 07:12:25 +0300

    bot

---
+++ bot.js
@@ -0,0 +1,12 @@
+{
+	"bots": [
+		{
+			"logic": "sd3",
+			"settings": "sd3_2018_01_16.properties",
+			"instrument": "BTC_USDT",
+			"interval": "ONE_MIN",
+			"limit": 0.9,
+			"trainStart": "2018-01-16T00:00:00Z[GMT]"	
+		}
+	]
+}
--- trading-bot/bot.sh
+++ trading-bot/bot.sh
@@ -2,4 +2,4 @@
 set -ue -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
-exec java -cp `cat trading-bot/target/trading-bot-1.0-SNAPSHOT.cp.txt`:trading-bot/target/trading-bot-1.0-SNAPSHOT.jar  ru.efreet.trading.bot.Main $@
+exec chrt  -i 0 java -cp `cat trading-bot/target/trading-bot-1.0-SNAPSHOT.cp.txt`:$DIR/..:trading-bot/target/trading-bot-1.0-SNAPSHOT.jar  ru.efreet.trading.bot.Main $@
