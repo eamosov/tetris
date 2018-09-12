90dcc836e18106f514938d03e540e9679b9d6bf4
Evgeny Amosov <efreet@efreet.ru>
2018-09-13 02:21:52 +0300

    Remove check for order while we dont need it

--- trading-common/src/main/kotlin/ru/efreet/trading/bot/Trader.kt
+++ trading-common/src/main/kotlin/ru/efreet/trading/bot/Trader.kt
@@ -123,7 +123,7 @@ class Trader(val tradeRecordDao: TradeRecordDao?,
         else if (td.closePrice > td.maxPrice)
             td.maxPrice = td.closePrice
 
-        cash.add(Pair(advice.bar.endTime, deposit((exchange is FakeExchange) || (logicAdvice.decision != Decision.NONE))))
+        cash.add(Pair(advice.bar.endTime, deposit((exchange is FakeExchange) || needForcedUpdate)))
 
 //        if (advice.decision == Decision.NONE &&
 //                lastBuy.containsKey(advice.instrument) &&
